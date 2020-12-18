2c728dccb0851fe6f8b5671fb95beaea1ae0ba28
Malini Vijayaraman <vmalini87@gmail.com>
2020-12-17 22:00:53 -0500

    dns updated

--- roboshop-project/components/shipping.sh
+++ roboshop-project/components/shipping.sh
@@ -39,7 +39,7 @@ STAT $? "Shipping Compile"
 chown roboshop:roboshop /home/roboshop/${COMPONENT} -R
 
 INFO "Configuring Shipping Startup Script "
-sed -i -e "s/CARTENDPOINT/cart-test.devopsb53.tk/" -e "s/DBHOST/mysql-test.devopsb53.tk/" /home/roboshop/${COMPONENT}/systemd.service
+sed -i -e "s/CARTENDPOINT/cart-test.devopstrainings01.tk/" -e "s/DBHOST/mysql-test.devopstrainings01.tk/" /home/roboshop/${COMPONENT}/systemd.service
 STAT $? "Startup script configuration"
 
 INFO "Setup SystemD Service for Shipping"
